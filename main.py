e02006cc80cb763186bb01ac48d9f6d344d9685f
Eliot Bitting <55938544+ElBitting@users.noreply.github.com>
2023-12-29 23:32:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+g = 9.81
+v0 = float(input("What is the initial velocity "))
+t = float(input("What time do you want to check "))
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+while t< 0:
+    print('Error, the time must be greater than zero.')
+    t = float(input("What is the time you want to check "))
 
+y = v0 * t - g * t ** 2 / 2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+if t > 2*v0/g or y == 0:
+    print("That ball is on the ground")
+else:
+    print('The ball is '+str(y)+' meters above the ground')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
